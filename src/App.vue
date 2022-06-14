60fa7b83be0794a200afae9c1e81f6a0733c7a41
Daniel Uboa <uboadaniel@gmail.com>
2022-06-14 22:22:59 +0300

    Code javascript methods

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,57 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, computed } from "vue";
+
+const player = ref("x");
+const board = ref([
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+]);
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+};
+
+const winner = computed(() => calculateWinner(board.value.flat()));
+
+const makeMove = (x, y) => {
+  if (winner.value) return;
+
+  if (board.value[x][y] != "") return;
+
+  board.value[x][y] = player.value;
+  player.value = player.value == "x" ? "o" : "x";
+};
+
+const resetGame = () => {
+  board.value = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  player.value = "x";
+};
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
